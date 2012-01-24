17caecc7de0b87dd1a5a713688ca4687902221a9
Adam Gandelman <adamg@canonical.com>
2012-01-24 11:06:05 -0800

    Update manager for new keystone management implementation

--- hooks/manager.py
+++ hooks/manager.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python
 #
 # Bootstraps the keystone API so we can utilize its managment API natively
+
 import keystone.manage
+import keystone.manage2
 import keystone.backends as db
 from keystone import version
 from keystone.common import config
+from keystone import config as new_config
 from keystone.manage import api
 from keystone.manage import RaisingOptionParser
+import utils
 
 parser = RaisingOptionParser("", version='%%prog %s'
          % version.version())
 (options, args) = config.parse_options(parser)
 _config_file, conf = config.load_paste_config('admin', options, args)
-db.configure_backends(conf.global_conf)
+
+# get config location from juju charm config
+config = utils.config_get()["config-file"]
+
+CONF = new_config.CONF
+
+CONF(config_files=[config])
+db.configure_backends()
